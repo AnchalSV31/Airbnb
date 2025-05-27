b637c331225a8e84c6818062fc81b7249505bacd
Anchal Shukla <shuklaanchal532@gmail.com>
2025-05-27 09:16:39 +0530

    Corrected name

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req, res) => {
             if(err){
                 return next(err);
             }
-            req.flash("success", "Welcome to Wanderhub!");
+            req.flash("success", "Welcome to WanderInn!");
             res.redirect("/listings");
         });
     }catch(e){
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async(req, res)=>{
-    req.flash("success","Welcome back to Wanderhub!");
+    req.flash("success","Welcome back to WanderInn!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
